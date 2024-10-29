6e121311cb10beb58573849b1750f9547e3c24ec
Admir Alicic <admir.alicic@gmail.com>
2024-10-29 15:33:46 +0100

    feat: setup vcs

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "admir-personal"
   type = string
   }
+
+  variable "github_app_installation_id" {
+    default = 56533247
+    type = number
+  }
+
+  variable "github_organization_name" {
+    default = "admiralicic"
+    type = string
+  }
